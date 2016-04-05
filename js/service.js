9e385fd118491682b9a15254f432a283cde4afa7
smart_he <946834832@qq.com>
2016-04-05 23:09:53 +0800

    s

--- js/service.js
+++ js/service.js
@@ -5,6 +5,14 @@ var app = angular.module('app.service',['ngResource']);
 app.factory('getData',[$resource,function($resource){
     return $resource('/recipe/:id',{id:'@id'});
 }]);
-app.factory('getDetaildata',['getData'],function(){
-
+app.factory('getDetaildata',['getData','$q'],function(getData,$q){
+    return function(){
+        var defer = $q.defer;
+        getData.query(function(recipes){
+            defer.resolve(recipes)
+        },function(err){
+            defer.reject(err)
+        });
+        return defer.promise
+    };
 });
