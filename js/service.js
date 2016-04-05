3d2e742c257359e1ee237d3f316dfd95cd98856a
smart_he <946834832@qq.com>
2016-04-05 23:35:35 +0800

    s

--- js/service.js
+++ js/service.js
@@ -2,17 +2,21 @@
  * Created by lenovo on 2016/4/2.
  */
 var app = angular.module('app.service',['ngResource']);
-app.factory('getData',[$resource,function($resource){
-    return $resource('/recipe/:id',{id:'@id'});
+app.factory('serviceRouter',['$resource',function($resource){
+    var BASEURL = "";
+    return function(url,param,method){
+        url+=BASEURL;
+        return $resource(url,param,method);
+    }
+}]);
+app.factory('getData',[serviceRouter,function(serviceRouter){
+    return {
+        login:function(){},
+        regist:function(){},
+        getArticle:function(){},
+        
+    }
 }]);
 app.factory('getDetaildata',['getData','$q'],function(getData,$q){
-    return function(){
-        var defer = $q.defer;
-        getData.query(function(recipes){
-            defer.resolve(recipes)
-        },function(err){
-            defer.reject(err)
-        });
-        return defer.promise
-    };
+
 });
