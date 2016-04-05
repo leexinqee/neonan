7115638d9dab8596909292f0525e3ae23969fccf
smart_he <946834832@qq.com>
2016-04-05 19:21:00 +0800

    s

--- js/service.js
+++ js/service.js
@@ -2,4 +2,20 @@
  * Created by lenovo on 2016/4/2.
  */
 var app = angular.module('app.service',['ngResource']);
-app.service();
+app.factory('getData',[$resource,function($resource){
+    return function(url,params,methods){
+        var BASEURL = "http://v2.api.neonan.com/v4/";
+        url+=BASEURL;
+        var defaults = {
+            get:{
+                method:'get',
+                isArray:false
+            },
+            post:{
+                method:'post'
+            }
+        };
+        methods = angular.extend(defaults,methods);
+        return resourse = $resource(url,params,methods);
+    };
+}]);
