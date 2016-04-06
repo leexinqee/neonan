5f5af9bd4d0fa84ac70aae5794cab21446d4258b
leexinqee <leexinqee@163.com>
2016-04-06 13:32:15 +0800

    other module

--- js/service.js
+++ js/service.js
@@ -2,7 +2,7 @@
  * Created by lenovo on 2016/4/2.
  */
 var app = angular.module('app.service',['ngResource']);
-app.factory('getData',[$resource,function($resource){
+app.factory('getData',["$resource", function($resource){
     return $resource('/recipe/:id',{id:'@id'});
 }]);
 app.factory('getDetaildata',['getData','$q'],function(getData,$q){
