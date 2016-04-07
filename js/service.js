5a62d4dc17af92ddec999c24ecd2facc587ba799
smart_he <946834832@qq.com>
2016-04-07 09:42:35 +0800

    s

--- js/service.js
+++ js/service.js
@@ -1,31 +1,31 @@
-/**
- * Created by lenovo on 2016/4/2.
- */
-var app = angular.module('app.service',['ngResource']);
-app.factory('serviceRouter',['$resource',function($resource){
-    var BASEURL = "";
-    return function(url,param,method){
-        url+=BASEURL;
-        return $resource(url,param,method);
-    }
-}]);
-app.factory('getData',[serviceRouter,function(serviceRouter){
-    return {
-        login:function(){
-            var defer = $q.defer();
-            serviceRouter.get({id:cardID},function(data,headers){
-                defer.resolve(data);
-            },function(data,headers){
-                defer.reject(data);
-            });
-            return defer.promise
-        },
-        regist:function(){},
-        findPwd:function(){},
-        getArticle:function(){},
-        getArticleDetail:function(){},
-        getMyCollectionArticle:function(){},
-        getAlbum:function(){},
-        getCategory:function(){}
-    }
-}]);
+///**
+// * Created by lenovo on 2016/4/2.
+// */
+//var app = angular.module('app.service',['ngResource']);
+//app.factory('serviceRouter',['$resource',function($resource){
+//    var BASEURL = "http://phptest.neonan.com";
+//    return function(url,param,method){
+//        url+=BASEURL;
+//        return $resource(url,param,method);
+//    }
+//}]);
+//app.factory('getData',["serviceRouter",function(serviceRouter){
+//    return {
+//        login:function(){
+//            var defer = $q.defer();
+//            serviceRouter.get({id:cardID},function(data,headers){
+//                defer.resolve(data);
+//            },function(data,headers){
+//                defer.reject(data);
+//            });
+//            return defer.promise
+//        },
+//        regist:function(){},
+//        findPwd:function(){},
+//        getArticle:function(){},
+//        getArticleDetail:function(){},
+//        getMyCollectionArticle:function(){},
+//        getAlbum:function(){},
+//        getCategory:function(){}
+//    }
+//}]);
